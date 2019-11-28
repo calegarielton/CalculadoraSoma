e3068faad0fb2c75499a7d8cb40398e43db201c9
EltonCalegari <calegarielton@gmail.com>
2019-11-28 02:09:30 -0300

    Added index.html and main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    function calcular(){
+    var numero1 = document.getElementById(numero1);
+    var numero2 = document.getElementById(numero2);
+    var soma = parseInt(numero1) + parseInt(numero2);
+    alert(soma);
+    }
+</script>
